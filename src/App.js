7819d0f1224789663867996433689bac407f6588
Joan Comadran <joan.comadran2@gmail.com>
2023-05-30 13:13:24 +0200

    plans

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
-import Hero from './components/Hero Section/Hero';
-import Programs from './components/Programs/Programs';
+import "./App.css";
+import Hero from "./components/Hero Section/Hero";
+import Programs from "./components/Programs/Programs";
+import Reasons from "./components/Reasons/Reasons";
+import Plans from "./components/Plans/Plans";
 
 function App() {
   return (
     <div className="App">
-          <Hero />
-          <Programs />
-
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans />
     </div>
   );
 }
